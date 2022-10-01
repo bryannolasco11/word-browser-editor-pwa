6ee4bc3917415c6ab26ebc39eb44af1c46ba511e
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-09-30 21:54:11 -0500

    added html plugin.  Also, logo shows up

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,6 +3,13 @@ import Editor from './editor';
 import './database';
 import '../css/style.css';
 
+// import logo
+import Logo from '../images/logo.png';
+
+window.addEventListener('load', function () {
+  document.getElementById('logo').src = Logo;
+})
+
 const main = document.querySelector('#main');
 main.innerHTML = '';
 
